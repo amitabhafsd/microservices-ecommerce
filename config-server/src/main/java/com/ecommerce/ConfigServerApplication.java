19c1be120fe1174eb5cfa5d8cd7973cf070e10cd
amitabhafsd <contact.amitabha.das@gmail.com>
2026-05-14 13:55:01 +0530

    config server infra setup

--- config-server/src/main/java/com/ecommerce/ConfigServerApplication.java
+++ config-server/src/main/java/com/ecommerce/ConfigServerApplication.java
@@ -3,8 +3,10 @@ package com.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
