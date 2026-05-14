af56be857b74ae88bb4da7cc91ef34defdf44d40
amitabhafsd <contact.amitabha.das@gmail.com>
2026-05-14 21:24:08 +0530

    auth changes again

--- auth-service/src/main/java/com/ecommerce/AuthServiceApplication.java
+++ auth-service/src/main/java/com/ecommerce/AuthServiceApplication.java
@@ -1,6 +1,8 @@
 
 package com.ecommerce;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
         SpringApplication.run(AuthServiceApplication.class, args);
     }
 }
